1d9186e62282b065763463047877d4d9b2cd10ec
GongZhaoyuan <140373573+GongZhaoyuan@users.noreply.github.com>
2023-07-25 12:25:24 +0800

    Update hyperparameters.py

--- configs/hyperparameters.py
+++ configs/hyperparameters.py
@@ -8,8 +8,8 @@ latent_ball_num_of_samples = 1
 locality_regularization_interval = 1
 use_locality_regularization = True
 regulizer_l2_lambda = 0.1
-regulizer_lpips_lambda = 0.1
-regulizer_alpha = 1
+regulizer_lpips_lambda = 10
+regulizer_alpha = 30
 
 ## Loss
 pt_l2_lambda = 1
